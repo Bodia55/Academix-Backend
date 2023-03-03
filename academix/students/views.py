0180f35fd55f4f7400a4d40ab0fd7ed740e7c057
ShadyH20 <shady.hani1@gmail.com>
2023-03-03 15:01:14 +0200

    added response headers

--- academix/students/views.py
+++ academix/students/views.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from .scrapper import Scrapper
 
+
 class StudentAPIView(ViewSet):
     def courses(self, request):
         if request.method == 'POST':
@@ -10,19 +11,24 @@ class StudentAPIView(ViewSet):
             password = request.data["password"]
             scrapper = Scrapper(username, password)
             courses, success = scrapper.get_courses_data()
-            return Response({'courses': courses, 'success': success})
+            return Response(data={'courses': courses, 'success': success}, headers={
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Accept': '*/*',
+            })
+
     def gpa(self, request):
         if request.method == 'POST':
             username = request.data["username"]
             password = request.data["password"]
             scrapper = Scrapper(username, password)
             gpa, success = scrapper.get_gpa_please('2022-2023')
-            return Response({'gpa': gpa, 'success':success})
+            return Response({'gpa': gpa, 'success': success})
+
     def idname(self, request):
         if request.method == 'POST':
             username = request.data["username"]
             password = request.data["password"]
             scrapper = Scrapper(username, password)
             idName, success = scrapper.get_idname()
-            return Response({'id': idName[0], 'name': idName[1], 'success':success})
-
+            return Response({'id': idName[0], 'name': idName[1], 'success': success})
